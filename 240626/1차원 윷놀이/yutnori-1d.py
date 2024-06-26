039b935ae97bf1a6f885a788fcab9e044381b45f
Heejay Kong <heejay.dev@gmail.com>
2024-06-26 20:29:05 +0900

    Title: 1차원 윷놀이, Time: 1915ms, Memory: 29MB, Status: Passed - CodeTree

---
+++ 240626/1차원 윷놀이/yutnori-1d.py
@@ -0,0 +1,52 @@
+# O(k^n * n)
+n,m,k = tuple(map(int, input().split()))
+distances = list(map(int, input().split()))
+
+
+def solution(destination, horse_count, distances):
+    ans = 0
+    arr = []
+    turns_count = len(distances)
+
+
+    def calc(chosen_arr):
+        # print(chosen_arr, end=", ")
+        nonlocal destination, horse_count
+
+        result = 0
+        scores = [0] + [1 for i in range(horse_count)]
+
+        for turn, horse in enumerate(chosen_arr):
+            distance = distances[turn]
+            scores[horse] += distance
+
+        # print(scores)
+        for score in scores:
+            if score >= destination:
+                result += 1
+
+        return result
+
+
+    def choose(curr_idx):
+        nonlocal horse_count, turns_count, ans
+
+        # 2. 만들어진 순열로 점수 계산하고 max값과 겨루기
+        if curr_idx == turns_count:
+            ans = max(ans, calc(arr[:]))
+            # print(ans)
+            return
+
+        for i in range(1, horse_count + 1):
+            arr.append(i)
+            choose(curr_idx + 1)
+            arr.pop()
+
+
+    # 1. 말들을 가지고 턴의 수만큼 중복순열 만들기
+    choose(0)
+
+    return ans
+
+
+print(solution(m, k, distances))
